cb668b66a5f388219711e6b6188d612dcaad6e27
hadimakki387 <hmakki387@gmail.com>
2023-12-31 16:36:35 +0200

    applied serverless

---
+++ netlify/functions/api.ts
@@ -0,0 +1,56 @@
+import bodyParser from "body-parser";
+import express, { Express } from "express";
+import helmet from "helmet";
+import cors from "cors";
+import xss from "xss-clean";
+import ExpressMongoSanitize from "express-mongo-sanitize";
+import compression from "compression";
+import httpStatus from "http-status";
+import router from "../../src/routes";
+import {
+  ApiError,
+  errorConverter,
+  errorHandler,
+} from "../../src/modules/errors";
+import serverless from "serverless-http";
+
+const app: Express = express();
+
+// set security HTTP headers
+app.use(helmet());
+
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
+
+// enable cors
+app.use(cors());
+app.options("*", cors());
+
+// parse json request body
+app.use(express.json());
+
+//parse urlencoded request body
+app.use(express.urlencoded({ extended: true }));
+
+// sanitize request data
+app.use(xss());
+app.use(ExpressMongoSanitize());
+
+// gzip compression
+app.use(compression());
+
+// api routes
+app.use("/api/", router);
+
+//return 404 for any unknown api
+app.use((_req, _res, next) => {
+  next(new ApiError(httpStatus.NOT_FOUND, "Not found"));
+});
+
+// convert errors to ApiErrors, if needed
+app.use(errorConverter);
+
+//handle error
+app.use(errorHandler);
+
+export const handler = serverless(app);
